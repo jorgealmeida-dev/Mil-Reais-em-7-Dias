6b991f9a81cedb9919459dc12e645c4994960f3f
jorgealmeida-dev <jorge999almeida@gmail.com>
2026-03-17 12:09:48 -0300

    Primeiro Commit-Projeto Responsividade

---
+++ script.js
@@ -0,0 +1,14 @@
+const form = document.querySelector(".fale-comosco");
+const mascara = document.querySelector(".mascara-formulario");
+
+function clickeiNoBotao() {
+  form.style.left = "50%";
+  form.style.transform = "translateX(-50%)";
+  mascara.style.visibility = "visible";
+}
+
+function fecharFormulario() {
+  form.style.left = "-300px";
+  form.style.transform = "translateX(0)";
+  mascara.style.visibility = "hidden";
+}
